e5f4b44dd2b932925c1cdc223535aa935129fb47
goda <goda@gmail.cn>
2025-05-26 18:00:42 +0800

    update background

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ import ChatbotWidget from './components/ChatbotWidget';
 
 const AppContainer = styled.div`
   min-height: 100vh;
-  background: url('https://images.unsplash.com/photo-1555710908-25866c9c0cf6?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D') center/cover no-repeat;
+  background: url('./bg.png') center/cover no-repeat;
   display: flex;
   align-items: center;
   justify-content: center;
