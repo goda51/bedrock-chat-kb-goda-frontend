499d2eed6c105e917933dc73c1c9820241a4fcb5
goda <goda@gmail.cn>
2025-05-27 13:23:00 +0800

    update background

--- src/App.tsx
+++ src/App.tsx
@@ -34,7 +34,7 @@ const gradientAnimation = keyframes`
 
 const AppContainer = styled.div`
   min-height: 100vh;
-  background: url('./bg.png') center/cover no-repeat;
+  background: url('https://images.unsplash.com/photo-1555710908-25866c9c0cf6?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D') center/cover no-repeat;
   display: flex;
   align-items: center;
   justify-content: center;
@@ -95,7 +95,7 @@ const App: React.FC = () => {
     <>
       <GlobalStyle />
       <AppContainer>
-        <ForegroundImage style={{ opacity: loaded ? 1 : 0, transition: 'opacity 1s ease-in-out' }} />
+        {/* <ForegroundImage style={{ opacity: loaded ? 1 : 0, transition: 'opacity 1s ease-in-out' }} /> */}
         <ContentWrapper>
           <ChatbotWidget />
         </ContentWrapper>
